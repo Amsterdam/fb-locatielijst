eaf0dec4bdf58b3eabe041ec05c1ad8a51adc686
landrej <user@example.org>
2023-11-23 12:33:55 +0100

    updates models

--- datafundament_fb/locations/models.py
+++ datafundament_fb/locations/models.py
@@ -14,17 +14,18 @@ class Location(models.Model):
     '''
     building_code = models.IntegerField(
         verbose_name='Pandcode', default=compute_building_code) # possible race condition when a location is added simultaneously; not worried about it now
-    short_name = models.CharField(verbose_name='Afkorting', max_length=12)
-    name = models.CharField(verbose_name='Locatie naam',max_length=100,)
+    short_name = models.CharField(verbose_name='Afkorting', max_length=12, null=True, blank=True)
+    name = models.CharField(verbose_name='Locatie',max_length=100,)
     description = models.CharField(
         verbose_name='Beschrijving', max_length=255)
     active = models.BooleanField(verbose_name='Actief', default=True)
     last_modified = models.DateField(verbose_name='Laatste wijziging', auto_now=True)
     street = models.CharField(verbose_name='Straat', max_length=100)
     street_number = models.IntegerField(verbose_name='Straatnummer')
+    street_number_letter = models.CharField(max_length=10, null=True, blank=True)
     street_number_extension = models.CharField(
-        verbose_name='Toevoeging', max_length=10, null=True, blank=True)
-    postal_code = models.CharField(verbose_name='Postcode', max_length=7)
+        verbose_name='Letter toevoeging', max_length=10, null=True, blank=True)
+    postal_code = models.CharField(verbose_name='Postcode', max_length=7) # aparte validatie voor postcode
     city = models.CharField(verbose_name='Plaats', max_length=100)
     construction_year = models.IntegerField(verbose_name='Bouwjaar', null=True, blank=True)
     floor_area = models.IntegerField(
@@ -40,14 +41,18 @@ class Location(models.Model):
 
     class Meta:
         verbose_name = 'Locatie'
-        constraints = [models.UniqueConstraint(
-            fields=['building_code'], name='unique_building_code')]
+        constraints = [
+            models.UniqueConstraint(fields=['building_code'], name='unique_building_code'),
+            models.UniqueConstraint(fields=['name'], name='unique_location_name')
+        ]
 
 
 class LocationPropertyType(models.TextChoices):
     BOOL = "BOOL", "Boolean"
-    INT = "INT", "Integer"
+    DATE = "DATE", "Datum"
+    INT = "INT", "Numeriek"
     TEXT = "TEXT", "Tekst"
+    LIST = "LIST", "Lijst" # Indicates related property option
 
 
 class LocationProperty(models.Model):
@@ -68,30 +73,44 @@ class LocationProperty(models.Model):
         verbose_name = 'Locatie eigenschap'
         verbose_name_plural = 'Locatie eigenschappen'
 
+    def __str__(self):
+        required = ', verplicht' if self.required else ''
+        multiple = ', meervoudig' if self.multiple else ''
+        return f'{self.label} ({self.property_type}){required}{multiple}'
+
 
 class PropertyOption(models.Model):
     '''
     Choice list for (some) custom entries 
     '''
-    location_property = models.ForeignKey(LocationProperty, on_delete=models.PROTECT)
+    location_property = models.ForeignKey(LocationProperty, on_delete=models.CASCADE)
     option = models.CharField(verbose_name='Optie', max_length=100)
 
     class Meta:
-        verbose_name = 'Eigenschap optielijst'
-        verbose_name_plural = 'Eigenschappen optielijsten'
+        verbose_name = 'Eigenschap optie'
+        verbose_name_plural = 'Eigenschappen opties'
+        constraints = [models.UniqueConstraint(
+            fields=['location_property','option'], name='unique_property_option')]
+
+    def __str__(self):
+        return f'{self.location_property}, {self.option}'
+
 
 class LocationData(models.Model):
     '''
     Holds each custom data entry for each location
     '''
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    location_property = models.ForeignKey(LocationProperty, on_delete=models.PROTECT)
+    location_property = models.ForeignKey(LocationProperty, on_delete=models.CASCADE)
     property_option = models.ForeignKey(PropertyOption, on_delete=models.PROTECT)
     value = models.CharField(verbose_name='Waarde', max_length=255)
 
     class Meta:
         verbose_name = 'Locatie gegeven'
         verbose_name_plural = 'Locatie gegevens'
+
+    def __str__(self):
+        return f'{self.location}, {self.location_property}, {self.property_option}, {self.value} '
 
 
 class ExternalService(models.Model):
@@ -104,6 +123,9 @@ class ExternalService(models.Model):
         verbose_name = 'Externe koppeling'
         verbose_name_plural = 'Externe koppelingen'
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class LocationExternalService(models.Model):
     '''
@@ -111,8 +133,11 @@ class LocationExternalService(models.Model):
     '''
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     external_service = models.ForeignKey(ExternalService, on_delete=models.CASCADE)
-    external_service_entity_id = models.CharField(verbose_name='Externe Id', max_length=100)
+    external_location_code = models.CharField(verbose_name='Externe locatie code', max_length=100)
 
     class Meta:
         verbose_name = 'Locatie koppeling'
         verbose_name_plural = 'Locatie koppeling'
+
+    def __str__(self):
+        return f'{self.location}, {self.external_service}, {self.external_code} '
