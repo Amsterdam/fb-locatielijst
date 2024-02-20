75063247192271033178172e092a00c2fab06238
landrej <j.landre@amsterdam.nl>
2024-02-20 10:25:12 +0100

    update test for help_docs

--- datafundament_fb/help_docs/tests.py
+++ datafundament_fb/help_docs/tests.py
@@ -11,21 +11,24 @@ class TestDocumentation(TestCase):
         self.client.force_login(User.objects.get_or_create(username='testuser', is_superuser=True, is_staff=True)[0])
     
     def test_documentation(self):
-        # get documentation instance
-        documentation = Documentation.objects.get(id=1)
+        # get documentation instance id
+        documentation_id = Documentation.objects.first().id
 
         # list page
         response = self.client.get(reverse('help_docs_urls:documentation-list'))
         self.assertEqual(response.status_code, 200)
         
         # detail page
-        response = self.client.get(reverse('help_docs_urls:documentation-detail', args=[1]))
+        response = self.client.get(reverse('help_docs_urls:documentation-detail', args=[documentation_id]))
         self.assertEqual(response.status_code, 200)
 
     def test_documentation_unauthorized(self):
         """Check if the form is only accessible for staff members"""
         # Ceate user with withou staff permissions
         self.client.force_login(User.objects.get_or_create(username='no_admin')[0])
+
+        # get documentation instance id
+        documentation_id = Documentation.objects.first().id
 
         # verify that access is not allowed and request is redirect to login page
         # list page
@@ -35,7 +38,7 @@ class TestDocumentation(TestCase):
         self.assertEqual(response.headers['Location'], '/admin/login/?next=' + url)
 
         # detail page
-        url = reverse('help_docs_urls:documentation-detail', args=[1])
+        url = reverse('help_docs_urls:documentation-detail', args=[documentation_id])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.headers['Location'], '/admin/login/?next=' + url)
