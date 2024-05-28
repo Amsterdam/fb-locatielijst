8918c71a5f3f403a6abff1fd76b66b1d06389c9b
landrej <j.landre@amsterdam.nl>
2024-05-28 13:28:02 +0200

    added tests

--- datafundament_fb/locations/templatetags/utils.py
+++ datafundament_fb/locations/templatetags/utils.py
@@ -7,11 +7,11 @@ def get_type(value):
     return type(value).__name__
 
 @register.filter
-def verbose_name(value, plural=None):
-    if plural == 'plural':
-        return value._meta.verbose_name_plural
-    return value._meta.verbose_name
+def verbose_name(model, plural=False):
+    if plural:
+        return model._meta.verbose_name_plural
+    return model._meta.verbose_name
 
 @register.filter
-def reverse_url(value, view):
-    return value._meta.model_name + "-" + view
+def reverse_url(instance, view):
+    return instance._meta.model_name + "-" + view
