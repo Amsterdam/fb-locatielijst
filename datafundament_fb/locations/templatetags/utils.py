12e33c06dcf38b5ba6a61f19c3fe0cdb20e527a3
landrej <j.landre@amsterdam.nl>
2024-05-29 09:47:01 +0200

    Feature/admin forms to custom forms (#54)
    * propertygroup views voor lijst en update form
    
    * external service list view and update form
    
    * locationproperty list view and base update form
    
    * basic crud and generic templates
    
    * breadcrumbs gelijktgetrokken
    
    * added succes message to forms
    
    * propertyoptions curd formuleren
    
    * aanpassingen aan templates
    
    * delete message, checks on choice properties
    
    * revert van template files die niet bij deze pr horen
    
    * nog 2 bestanden gerevert
    
    * added tests
    
    * admin templates verwijderd

--- datafundament_fb/locations/templatetags/utils.py
+++ datafundament_fb/locations/templatetags/utils.py
@@ -5,3 +5,13 @@ register = template.Library()
 @register.filter
 def get_type(value):
     return type(value).__name__
+
+@register.filter
+def verbose_name(model, plural=False):
+    if plural:
+        return model._meta.verbose_name_plural
+    return model._meta.verbose_name
+
+@register.filter
+def reverse_url(instance, view):
+    return instance._meta.model_name + "-" + view
--- datafundament_fb/locations/tests/test_utils.py
+++ datafundament_fb/locations/tests/test_utils.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from locations.templatetags.utils import get_type
-
+from locations.templatetags.utils import get_type, verbose_name, reverse_url
+from locations.models import Location
+from django.db import models
 class LocationUtilsTest(TestCase):
     """
     Test Utils
@@ -17,3 +18,17 @@ class LocationUtilsTest(TestCase):
         self.assertEqual(get_type(dict), 'dict')
         self.assertEqual(get_type(None), 'NoneType')
 
+    def test_verbose_name(self):
+        """Test returning the verbose name of a model"""
+
+        # Test single name
+        self.assertEqual(verbose_name(Location), 'Locatie')
+        # Test plural name
+        self.assertEqual(verbose_name(Location, True), 'Locaties')
+
+    def test_reverse_url(self):
+        """Test reverse url path from instance"""
+
+        # Test list view and empty view
+        self.assertEqual(reverse_url(Location, 'list'), 'location-list')
+        self.assertEqual(reverse_url(Location, ''), 'location-')
