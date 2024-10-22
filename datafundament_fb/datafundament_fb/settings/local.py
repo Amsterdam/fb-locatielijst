65200af0d9face3728e02412a79ec1d860317e49
landrej <j.landre@amsterdam.nl>
2024-10-22 12:28:58 +0200

    added local environment for development and tests

---
+++ datafundament_fb/datafundament_fb/settings/local.py
@@ -0,0 +1,13 @@
+"""
+Settings for a development environment
+"""
+
+from pathlib import Path
+import os
+
+# Load the default settings from base.py
+from datafundament_fb.settings.base import *
+
+# Settings specific to this enviromnent
+AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
+LOGIN_URL = '/auth/login'
