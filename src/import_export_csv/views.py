a67df90cac30a4b08ec5e187c5bcb0ed233cacfd
ferhany <f.yildiz2002@gmail.com>
2026-03-30 09:31:48 +0200

    Remove comment and move LocationExportView to the bottom

--- src/import_export_csv/views.py
+++ src/import_export_csv/views.py
@@ -7,20 +7,6 @@ from import_export_csv.forms import LocatieImportForm
 from import_export_csv.handle_import import handle_import_csv
 from .exporter import get_csv_response, fetch_locations_for_export
 
-class LocationExportView(LoginRequiredMixin, View):
-    template = "import_export_csv/locatie-export.html"
-
-    def get(self, request, *args, **kwargs):
-        if request.GET:
-            all_locations = fetch_locations_for_export()
-            return get_csv_response(all_locations)
-        return render(request=request, template_name=self.template)
-
-    def post(self, request, *args, **kwargs):
-        all_locations = fetch_locations_for_export()
-        return get_csv_response(all_locations)
-
-
 class IsStaffMixin(UserPassesTestMixin):
     def test_func(self):
         return self.request.user.is_staff
@@ -53,26 +39,15 @@ class LocatieImportView(LoginRequiredMixin, IsStaffMixin, View):
             messages.add_message(request, messages.SUCCESS, message)
         return render(request, template_name=self.template_name, context=context)
 
+class LocationExportView(LoginRequiredMixin, View):
+    template = "import_export_csv/locatie-export.html"
 
-# class LocationExportView(LoginRequiredMixin, View):
-#     template = "import_export_csv/location-export.html"
+    def get(self, request, *args, **kwargs):
+        if request.GET:
+            all_locations = fetch_locations_for_export()
+            return get_csv_response(all_locations)
+        return render(request=request, template_name=self.template)
 
-#     def get(self, request, *args, **kwargs):
-#         # when a query is given, return the csv file not the webpage
-#         if request.GET:
-#             # # Get a QuerySet of filtered locations
-#             # locations = Location.objects.search_filter(params=self.request.GET.dict(), user=self.request.user)
-#             # # Set the response with the csv file
-#             # response = get_csv_file_response(request, locations)
-#             print("testGet")
-#         else:
-#             response = render(request=request, template_name=self.template)
-#         return response
-
-#     def post(self, request, *args, **kwargs):
-#         # # Get all Location instances()
-#         # locations = Location.objects.search_filter(params=dict(), user=self.request.user)
-#         # # Set the response with the csv file
-#         # response = get_csv_file_response(request, locations)
-#         print("exporting!!")
-#         return response
+    def post(self, request, *args, **kwargs):
+        all_locations = fetch_locations_for_export()
+        return get_csv_response(all_locations)
