44a5027070b620f6c51ccbbfae83809fe7fa34b9
ferhany <f.yildiz2002@gmail.com>
2026-05-18 14:01:20 +0200

    Improve readability

--- src/fblocatie/querysets.py
+++ src/fblocatie/querysets.py
@@ -21,10 +21,11 @@ TRUE_STRINGS = {"ja", "j", "true", "1", "yes", "y"}
 FALSE_STRINGS = {"nee", "n", "false", "0", "no"}
 
 def _any_icontains(term: str, lookups: tuple[str, ...]) -> Q:
-    q = Q()
+    """Return a Query that ORs icontains filters for the given term across all provided lookups."""
+    query = Q()
     for lookup in lookups:
-        q |= Q(**{lookup: term})
-    return q
+        query |= Q(**{lookup: term})
+    return query
 
 def _person_name_match(term: str, prefix: str) -> Q:
     return (
@@ -52,48 +53,48 @@ class LocatieQuerySet(QuerySet):
         search_value = _extract_search_term(params)
         archive_value = (params.get("archive") or "").strip()
 
-        qs = self
-        qfilter = Q()
+        query_set = self
+        queryfilter = Q()
         needs_distinct = False
 
         if search_value:
             if property_value == "":
-                qfilter &= _any_icontains(search_value, DEFAULT_TEXT_LOOKUPS)
+                queryfilter &= _any_icontains(search_value, DEFAULT_TEXT_LOOKUPS)
                 if search_value.isdigit():
                     for field in DEFAULT_INT_LOOKUPS:
-                        qfilter |= Q(**{field: int(search_value)})
+                        queryfilter |= Q(**{field: int(search_value)})
             elif property_value in INT_FIELD_LOOKUPS:
                 if not search_value.isdigit():
-                    return qs.none()
-                qfilter &= Q(**{INT_FIELD_LOOKUPS[property_value]: int(search_value)})
+                    return query_set.none()
+                queryfilter &= Q(**{INT_FIELD_LOOKUPS[property_value]: int(search_value)})
             elif property_value in TEXT_FIELD_LOOKUPS:
-                qfilter &= Q(**{TEXT_FIELD_LOOKUPS[property_value]: search_value})
+                queryfilter &= Q(**{TEXT_FIELD_LOOKUPS[property_value]: search_value})
             elif property_value in FOREIGN_KEY_LOOKUPS:
                 id_lookup, name_lookup = FOREIGN_KEY_LOOKUPS[property_value]
-                qfilter &= Q(**{id_lookup: int(search_value)}) if search_value.isdigit() else Q(**{name_lookup: search_value})
+                queryfilter &= Q(**{id_lookup: int(search_value)}) if search_value.isdigit() else Q(**{name_lookup: search_value})
             elif property_value in MANY_TO_MANY_LOOKUPS:
                 id_lookup, name_lookup = MANY_TO_MANY_LOOKUPS[property_value]
                 needs_distinct = True
-                qfilter &= Q(**{id_lookup: int(search_value)}) if search_value.isdigit() else Q(**{name_lookup: search_value})
+                queryfilter &= Q(**{id_lookup: int(search_value)}) if search_value.isdigit() else Q(**{name_lookup: search_value})
             elif property_value in PERSON_LOOKUP_PREFIXES:
                 needs_distinct = True
-                qfilter &= _person_name_match(search_value, PERSON_LOOKUP_PREFIXES[property_value])
+                queryfilter &= _person_name_match(search_value, PERSON_LOOKUP_PREFIXES[property_value])
             elif property_value in {"vvo", "bvo"}:
                 try:
                     val = Decimal(search_value)
                 except InvalidOperation:
-                    return qs.none()
-                qfilter &= Q(**{("vastgoed__vvo" if property_value == "vvo" else "vastgoed__bvo"): val})
+                    return query_set.none()
+                queryfilter &= Q(**{("vastgoed__vvo" if property_value == "vvo" else "vastgoed__bvo"): val})
             elif property_value == "ambtenaar":
                 val = search_value.lower()
                 if val in TRUE_STRINGS:
-                    qfilter &= Q(ambtenaar=True)
+                    queryfilter &= Q(ambtenaar=True)
                 elif val in FALSE_STRINGS:
-                    qfilter &= Q(ambtenaar=False)
+                    queryfilter &= Q(ambtenaar=False)
                 else:
-                    return qs.none()
+                    return query_set.none()
             elif property_value == "adrs_toeg":
-                qfilter &= _any_icontains(
+                queryfilter &= _any_icontains(
                     search_value,
                     (
                         "bezoekadres__straat__icontains",
@@ -102,18 +103,18 @@ class LocatieQuerySet(QuerySet):
                     ),
                 )
             elif property_value == "gv_grp":
-                qfilter &= _any_icontains(search_value, ("vastgoed__GV_key__icontains", "vastgoed__gv_id__icontains"))
+                queryfilter &= _any_icontains(search_value, ("vastgoed__GV_key__icontains", "vastgoed__gv_id__icontains"))
             else:
-                return qs.none()
+                return query_set.none()
 
         # Archive filtering
-        qfilter &= filter_on_archive(archive_value)
+        queryfilter &= filter_on_archive(archive_value)
 
         # Non-staff users only see active locations
         if not user.is_staff:
-            qfilter &= Q(archief=False)
+            queryfilter &= Q(archief=False)
 
-        qs = qs.filter(qfilter)
+        qs = query_set.filter(queryfilter)
         if needs_distinct:
             qs = qs.distinct()
         return qs
